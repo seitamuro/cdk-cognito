d6775b36763990d7d889efaa8b0cdd0da3025caf
seitamuro <seitamuro0210@gmail.com>
2024-07-23 22:48:02 +0900

    Initial commit

---
+++ test/cdk-user-id-pool.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkUserIdPool from '../lib/cdk-user-id-pool-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-user-id-pool-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkUserIdPool.CdkUserIdPoolStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
